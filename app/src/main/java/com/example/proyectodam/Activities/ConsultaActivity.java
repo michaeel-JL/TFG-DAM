476f012d8c699666c007403443c47d48364f3261
Mikelito <maikell_joel_@outlook.es>
2021-04-11 12:30:44 +0200

    Activity CONSULTA: fragments realizados

--- app/src/main/java/com/example/proyectodam/Activities/ConsultaActivity.java
+++ app/src/main/java/com/example/proyectodam/Activities/ConsultaActivity.java
@@ -3,17 +3,30 @@ package com.example.proyectodam.Activities;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
+import com.example.proyectodam.Adapter.PageAdapter;
 import com.example.proyectodam.R;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -24,6 +37,10 @@ public class ConsultaActivity extends AppCompatActivity {
     private DatabaseReference mDataBase;
     private StorageReference mStorage;
     private FirebaseUser user;
+
+    private FirebaseUser userActual = FirebaseAuth.getInstance().getCurrentUser();
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference ref_user = database.getReference("Usuarios").child(user.getUid());
     private FirebaseAuth mAuth;
     private Uri uri;
 
@@ -33,6 +50,36 @@ public class ConsultaActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.consulta_activity);
+
+        ViewPager2 viewPager2 = findViewById(R.id.viewPager);
+        viewPager2.setAdapter(new PageAdapter(this));
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(tabLayout, viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                switch(position){
+
+                    case 0: {
+                        tab.setText("Users");
+                        tab.setIcon(R.drawable.ic_usuarios);
+                        break;
+                    }
+                    case 1: {
+                        tab.setText("Chats");
+                        tab.setIcon(R.drawable.ic_chats);
+                        break;
+                    }
+
+                }
+
+            }
+        });
+        tabLayoutMediator.attach();
+
+        final FirebaseUser user2 = FirebaseAuth.getInstance().getCurrentUser();
+
+        //userunico();
 
 
         //Obtenemos el usuario cuya sesión está abierta
@@ -67,5 +114,23 @@ public class ConsultaActivity extends AppCompatActivity {
                         return false;
                     }
                 });
+
+
     }
+/*
+    private void userunico() {
+
+        ref_user.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(!dataSnapshot.){
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }*/
 }
